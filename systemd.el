0e38df46761e02383fa40762a66ab6c0a2067674
Mario Lang <mlang@delysid.org>
2016-07-05 12:44:27 +0200

    `systemctl-list-units'.

--- systemd.el
+++ systemd.el
@@ -37,5 +37,31 @@
 (systemd-define "timedate1")
 (systemd-define "machine1")
 
+(define-derived-mode systemctl-list-units-mode tabulated-list-mode "Units"
+  "Major mode for displaying a list of Systemd Units."
+  (setq tabulated-list-format (vector (list "Unit" 30 t)
+				      (list "Descritpion" 20 nil)
+				      (list "Active" 8 t)
+				      (list "Loaded" 8 t)
+				      (list "State" 8 t)))
+  (tabulated-list-init-header))
+  
+(defun systemctl-list-units ()
+  "Display a list of all Systemd Units."
+  (interactive)
+  (with-current-buffer (get-buffer-create "*Systemd Units*")
+    (systemctl-list-units-mode)
+    (setq tabulated-list-entries
+	  (mapcar (lambda (desc)
+		    (list (nth 6 desc)
+			  (vector (nth 0 desc)
+				  (nth 1 desc)
+				  (nth 2 desc)
+				  (nth 3 desc)
+				  (nth 4 desc))))
+		  (systemd-ListUnits)))
+    (tabulated-list-print)
+    (pop-to-buffer (current-buffer))))
+
 (provide 'systemd)
 ;;; systemd.el ends here
